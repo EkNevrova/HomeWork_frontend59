cbc37f8c1bd2952ef2f80fd1810361aed1345ec5
EkNevrova <ek.nevrova@mail.ru>
2023-09-08 17:35:50 +0300

    HW 17 week

---
+++ index.js
@@ -0,0 +1,80 @@
+const data = [
+    {
+        id: 1,
+        type: 'car',
+        brand: 'Audi',
+        doors: 4,
+        price: 4300000,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/2020_Audi_e-Tron_Sport_50_Quattro.jpg/1200px-2020_Audi_e-Tron_Sport_50_Quattro.jpg'
+    },
+    {
+        id: 2,
+        type: 'car',
+        brand: 'Mercedes-Benz',
+        doors: 4,
+        price: 2800000,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg/300px-2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg'
+    },
+    {
+        id: 3,
+        type: 'bike',
+        brand: 'Harley-Davidson',
+        maxSpeed: 210,
+        price: 1300000,
+        image: 'https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2022/2022-iron-883/2022-iron-883-016/2022-iron-883-016-motorcycle.jpg'
+    },
+    {
+        id: 4,
+        type: 'bike',
+        brand: 'Harley-Davidson',
+        maxSpeed: 220,
+        price: 1400000,
+        image: 'https://cdn.dealerspike.com/imglib/products/harley-showroom/2020/livewire/main/Vivid-Black-Main.png'
+    }
+];
+
+class Transport {
+    constructor(type, price, brand) {
+        this.type = type;
+        this.price = price;
+        this.brand = brand;
+    }
+    getInfo() {
+        console.log(`type: ${this.type}, brand: ${this.brand}`);
+        return {
+            type: this.type,
+            brand: this.brand,
+        }
+    }
+    getPrice() {
+        console.log(`price: ${this.price}`);
+        return {
+            price: this.price,
+        }
+    }
+}
+
+class Car extends Transport {
+    constructor(type, price, brand) {
+        super(type, price, brand);
+    }
+    getDoorsCount() {
+        console.log(`doors: ${this.doors}`);
+        return {
+            doors: this.doors,
+        }
+    }
+}
+
+class Bike extends Transport {
+    constructor(type, price, brand) {
+        super(type, price, brand);
+    }
+    getMaxSpeed() {
+        console.log(`maxSpeed: ${this.maxSpeed}`);
+        return {
+            maxSpeed: this.maxSpeed,
+        }
+    }
+}
+
